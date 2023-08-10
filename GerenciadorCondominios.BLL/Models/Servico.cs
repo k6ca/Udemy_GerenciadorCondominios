765848f0b486a523e155e147a23d201581d1aff9
Hermes <eduardod.vieira@gmail.com>
2023-08-10 11:04:54 -0300

    20230810 - Class 4

---
+++ GerenciadorCondominios.BLL/Models/Servico.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GerenciadorCondominios.BLL.Models
+{
+	public class Servico()
+	{
+		public int ServicoId { get; set; }
+		public string Nome { get; set; }
+		public decimal Valor { get; set; }
+		public StatusServico Status { get; set; }
+		public string UsuarioId { get; set; }
+		public virtual Usuario Usuario { get; set; }
+		public ICollection<ServicoPredio> ServicoPredio { get; set; }
+	}
+
+	public enum StatusServico
+	{
+		Pendente, Recusado, Aceito
+	}
+}
