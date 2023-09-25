f7d2dd7f594912dcb3c3f0fe95b21b4af91816c0
k6ca <eduardod.vieira@gmail.com>
2023-09-25 01:23:32 -0300

    20230924 - Class 5

---
+++ GerenciadorCondominios.DAL/Mapeamentos/FuncaoMap.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GerenciadorCondominios.BLL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GerenciadorCondominios.DAL.Mapeamentos
+{
+    public class FuncaoMap : IEntityTypeConfiguration<Funcao>
+
+    {
+        void IEntityTypeConfiguration<Funcao>.Configure(EntityTypeBuilder<Funcao> builder)
+        {
+            builder.Property(f => f.Id).ValueGeneratedOnAdd();
+            builder.Property(f => f.Descricao).IsRequired().HasMaxLength(30);
+
+            builder.HasData(
+                new Funcao
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Morador",
+                    NormalizedName = "MORADOR",
+                    Descricao = "Morador do Prédio"
+                },
+                new Funcao
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Síndico",
+                    NormalizedName = "SÍNDICO",
+                    Descricao = "Síndico do Prédio"
+                }
+                new Funcao
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Administrador",
+                    NormalizedName = "ADMINISTRADOR",
+                    Descricao = "Administrador do Prédio"
+                });
+
+            builder.ToTable("Funcoes");
+
+        }
+    }
+}
---
+++ GerenciadorCondominios.DAL/Mapeamentos/MesMap.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GerenciadorCondominios.BLL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GerenciadorCondominios.DAL.Mapeamentos
+{
+    public class MesMa_p : IEntityTypeConfiguration<Mes>
+    {
+        public void Configure(EntityTypeBuilder<Mes> builder)
+        {
+            builder.HasKey(m => m.MesID);
+            builder.Property(m => m.MesID).ValueGeneratedNever();
+
+            builder.Property(m => m.Nome).IsRequired().HasMaxLength(30);
+            builder.HasIndex(m => m.Nome).IsUnique();
+
+            builder.HasMany(m => m.Alugueis).WithOne(m => m.Mes);
+            builder.HasMany(m => m.HistoricoRecursos).WithOne(m => m.Mes);
+
+            builder.HasData(
+                new Mes
+                {
+                    MesID = 1,
+                    Nome = "Janeiro"
+                },
+
+                new Mes
+                {
+                    MesID = 2,
+                    Nome = "Fevereiro"
+                },
+
+                new Mes
+                {
+                    MesID = 3,
+                    Nome = "Março"
+                },
+
+                new Mes
+                {
+                    MesID = 4,
+                    Nome = "Abril"
+                },
+
+                new Mes
+                {
+                    MesID = 5,
+                    Nome = "Maio"
+                },
+
+                new Mes
+                {
+                    MesID = 6,
+                    Nome = "Junho"
+                },
+
+                new Mes
+                {
+                    MesID = 7,
+                    Nome = "Julho"
+                },
+
+                new Mes
+                {
+                    MesID = 8,
+                    Nome = "Agosto"
+                },
+                
+                new Mes
+                {
+                    MesID = 9,
+                    Nome = "Setembro"
+                },
+
+                new Mes
+                {
+                    MesID = 10,
+                    Nome = "Outubro"
+                },
+
+                new Mes
+                {
+                    MesID = 11,
+                    Nome = "Novembro"
+                },
+
+                new Mes
+                {
+                    MesID = 12,
+                    Nome = "Dezembro"
+                }
+                );
+
+            builder.ToTable("Meses");
+        }
+    }
+}
